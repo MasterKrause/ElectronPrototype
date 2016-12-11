ed8a402ee913f1fa4ed06463252850314dbacf6b
Christopher Krause <=>
2016-12-11 11:53:36 -0500

    Working with Knockout

--- main.js
+++ main.js
@@ -14,16 +14,19 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
+  
+  // removes the menu bar
+  mainWindow.setMenu(null);
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, '/views/index.html'),
     protocol: 'file:',
     slashes: true
   }))
 
   // Open the DevTools.
- // mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
---
+++ viewModels/AppViewModel.js
@@ -0,0 +1,17 @@
+var ko = require('knockout');
+
+function AppViewModel(){
+    this.firstName = ko.observable("Bert");
+    this.lastName = ko.observable("Bertington");
+
+    this.fullName = ko.computed(function(){
+        return this.firstName() + " " + this.lastName(); 
+    }, this);
+
+    this.capitalizeLastName = function(){
+        var currentVal = this.lastName();
+        this.lastName(currentVal.toUpperCase());
+    }
+}
+
+ko.applyBindings(new AppViewModel());
